155e6563758a9acd0339f5a17ff0916c974dedec
mochandri <72057874+mochandri@users.noreply.github.com>
2022-05-21 22:51:16 +0700

    tambah barang

--- ci3/application/views/admin/data_barang.php
+++ ci3/application/views/admin/data_barang.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <button class="btn btn-sm btn-primary"><i class="fas fa-plus fa-fm"></i>Tambah Kue</button>
+    <button class="btn btn-sm btn-primary mb-3" data-toggle="modal" data-target="#tambah_barang"><i class="fas fa-plus fa-fm"></i>Tambah Kue</button>
 
 <table class="table table-bordered">
     <tr>
@@ -29,4 +29,55 @@
     
     <?php endforeach; ?>
 </table>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="tambah_barang" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Form Input Kue</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?php echo base_url(). 'admin/data_barang/tambah_aksi';?>" method="post" enctype="multipart/form-data">
+            <div class="form=group">
+                <label>Nama Barang</label>
+                <input type="text" name="nama_brg" class="form-control">
+
+            </div>
+            <div class="form=group">
+                <label>Keterangan</label>
+                <input type="text" name="keterangan" class="form-control">
+
+            </div>
+            <div class="form=group">
+                <label>Kategori</label>
+                <input type="text" name="kategori" class="form-control">
+
+            </div>
+            <div class="form=group">
+                <label>Harga</label>
+                <input type="text" name="harga" class="form-control">
+
+            </div>
+            <div class="form=group">
+                <label>Stok</label>
+                <input type="text" name="stok" class="form-control">
+
+            </div>
+            <div class="form=group">
+                <label>Gambar Kue</label>
+                <input type="file" name="gambar" class="form-control">
+
+            </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+    </form>
+    </div>
+  </div>
 </div>
--- ci3/application/views/dashboard.php
+++ ci3/application/views/dashboard.php
@@ -17,9 +17,8 @@
     <div class="row text-center mt-4">
 
     <?php
-        foreach ($barang as $brg) :
-?>
-        <div class="card ml-3" style="width: 18rem;">
+        foreach ($barang as $brg) :?>
+        <div class="card ml-3 mb-3" style="width: 16rem;">
         <img src="<?php echo base_url().'/uploads/' .$brg->gambar ?> " class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
