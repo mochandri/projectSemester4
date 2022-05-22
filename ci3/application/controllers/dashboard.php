49777ef2cfb4de1cadccb9c9f95a9b3661dbb6e3
E31201199_Ajeng Nur Khorisa <72060793+Ajeng721@users.noreply.github.com>
2022-05-22 17:10:27 +0700

    hapus keranjang, Lanjutkan Belanja, Pembayaran, Pesan

--- ci3/application/controllers/dashboard.php
+++ ci3/application/controllers/dashboard.php
@@ -30,5 +30,28 @@ class dashboard extends CI_Controller{
         $this->load->view('templates/footer');
 
     }
+
+    public function hapus_keranjang()
+    {
+        $this->cart->destroy();
+        redirect('dashboard/index');
+    }
+
+    public function pembayaran()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('pembayaran');
+        $this->load->view('templates/footer');
+    }
+
+    public function proses_pesanan()
+    {
+        $this->cart->destroy();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('proses_pesanan');
+        $this->load->view('templates/footer');
+    }
     
 }
