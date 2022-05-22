4605fb23de17d8e4f5dea3860069350de13ca0ea
mochandri <72057874+mochandri@users.noreply.github.com>
2022-05-22 19:22:22 +0700

    Update dashboard.php

--- ci3/application/views/dashboard.php
+++ ci3/application/views/dashboard.php
@@ -25,7 +25,7 @@
             <small><?php echo $brg->keterangan ?></small><br>
             <span class="badge text-bg-success mb-3">Rp.<?php 
             echo number_format($brg->harga,0, ',','.') ?></span><br>
-            <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg->id_brg,'<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>')?>
+            <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg->id_brg,'<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
             <a href="#" class="btn btn-sm btn-success">Detail</a>
         </div>
         </div>
