3899101f117b6e3335f404fdea3c74e2854042a4
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-05-22 19:45:49 +0700

    Update dashboard.php

--- ci3/application/controllers/dashboard.php
+++ ci3/application/controllers/dashboard.php
@@ -10,18 +10,25 @@ class dashboard extends CI_Controller{
         $this->load->view('dashboard', $data);
         $this->load->view('templates/footer');
     }
-    public function tambah_ke_keranjang($id){
+    public function tambah_ke_keranjang($id)
+    {
         $barang = $this->model_barang->find($id);
+       
         $data = array(
+
             'id'    =>  $barang->id_brg,
             'qty'   =>  1,
             'price' =>  $barang->harga,
             'name'  =>  $barang->nama_brg
 
 
-        );
-        $this->cart->insert($data);
-        redirect('dashboard');
+            
+            
+    );
+    
+    $this->cart->insert($data);
+    redirect('dashboard');
+
     }
     public function detail_keranjang(){
         $this->load->view('templates/header');
