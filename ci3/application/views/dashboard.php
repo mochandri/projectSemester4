753030c3f7c898591d1a99853e87b89b86078aed
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-05-22 19:45:43 +0700

    Update dashboard.php

--- ci3/application/views/dashboard.php
+++ ci3/application/views/dashboard.php
@@ -23,10 +23,14 @@
         <div class="card-body">
             <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
             <small><?php echo $brg->keterangan ?></small><br>
+
             <span class="badge badge-pill badge-success mb-3">Rp.
             <?php echo number_format($brg->harga,0, ',','.') ?></span>
             <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg
             ->id_brg,'<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>')?>
+
+           
+
             <a href="#" class="btn btn-sm btn-success">Detail</a>
         </div>
         </div>
