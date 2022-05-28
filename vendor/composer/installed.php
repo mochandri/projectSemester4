adb92d159f7a1ea97100eb594ef5997a20681c4d
mochandri <72057874+mochandri@users.noreply.github.com>
2022-05-28 16:55:37 +0700

    perbaikan

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'chriskacerguis/codeigniter-restserver' => array(
+            'pretty_version' => '3.1.4',
+            'version' => '3.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chriskacerguis/codeigniter-restserver',
+            'aliases' => array(),
+            'reference' => 'f386f4e0013daadaa213681a0a41af69aa81d9de',
+            'dev_requirement' => false,
+        ),
+    ),
+);
