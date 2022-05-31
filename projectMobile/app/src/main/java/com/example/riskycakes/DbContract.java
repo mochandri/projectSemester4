be7100180b87e1fc1c662cf811034ab50c287e59
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-06-01 01:38:12 +0700

    penambahan url Api

--- projectMobile/app/src/main/java/com/example/riskycakes/DbContract.java
+++ projectMobile/app/src/main/java/com/example/riskycakes/DbContract.java
@@ -1,6 +1,6 @@
 package com.example.riskycakes;
 
 public class DbContract {
-    public static final String SERVER_LOGIN_URL="";
-    public static final String SERVER_REGISTER_URL="";
+    public static final String SERVER_LOGIN_URL="https://7cde-103-144-18-67.ap.ngrok.io/projectSemester4/ci3/api/user";
+    public static final String SERVER_REGISTER_URL="https://7cde-103-144-18-67.ap.ngrok.io/projectSemester4/ci3/api/user";
 }
