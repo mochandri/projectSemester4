01c81ecc7741309c3f571a6684f153c748793b3b
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-06-01 01:25:01 +0700

    login dan register

---
+++ projectMobile/app/src/main/java/com/example/riskycakes/DbContract.java
@@ -0,0 +1,6 @@
+package com.example.riskycakes;
+
+public class DbContract {
+    public static final String SERVER_LOGIN_URL="";
+    public static final String SERVER_REGISTER_URL="";
+}
---
+++ projectMobile/app/src/main/java/com/example/riskycakes/LoginActivity.java
@@ -0,0 +1,115 @@
+package com.example.riskycakes;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText Username, Password;
+    Button login, register;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Username = (EditText) findViewById(R.id.edt_usernameLogin);
+        Password = (EditText) findViewById(R.id.edt_passwordLogin);
+        login = (Button) findViewById(R.id.btn_loginLogin);
+        register = (Button) findViewById(R.id.btn_registerLogin);
+
+        progressDialog = new ProgressDialog(LoginActivity.this);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sUsername = Username.getText().toString();
+                String sPassword = Password.getText().toString();
+
+                CheckLogin(sUsername, sPassword);
+            }
+        });
+    }
+
+    public void CheckLogin(final String Username, final String Password) {
+        if (checkNetworkConnection()) {
+            progressDialog.show();
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, DbContract.SERVER_LOGIN_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                String resp = jsonObject.getString("server_response");
+                                if (resp.equals("[{\"status\":\"OK\"}]")) {
+                                    Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
+
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }) {
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("Username", Username);
+                    params.put("Password", Password);
+                    return params;
+                }
+            };
+            VolleyConnection.getInstance(LoginActivity.this).addToRequestQue(stringRequest);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog.cancel();
+                }
+            }, 2000);
+        } else {
+            Toast.makeText(getApplicationContext(), "Tidak Ada Koneksi Internet", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    
+
+}
