92e1aec573fb5e3f4fe5baf26788892d94405117
mochandri <72057874+mochandri@users.noreply.github.com>
2022-06-03 06:35:35 +0700

    perbaikan

--- rizky/app/src/main/java/com/example/rizky/registerActivity.java
+++ rizky/app/src/main/java/com/example/rizky/registerActivity.java
@@ -1,21 +1,22 @@
 package com.example.rizky;
 
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,51 +24,53 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class registerActivity extends AppCompatActivity {
 
-    private EditText Username, Password;
-    private Button Login;
-    private TextView Register;
-    private static String URL_REGIST =" http://10.209.58.41/projectSemester4/ci3/api/user";
+public class registerActivity extends AppCompatActivity implements View.OnClickListener {
+        private EditText user, pass;
+        private FloatingActionButton btnRegis;
+        private Button btnLogin;
+
+        @Override
+        protected void onCreate(Bundle savedIntanceState) {
+            super.onCreate(savedIntanceState);
+            setContentView(R.layout.register_main);
+
+            user = (EditText) findViewById(R.id.et_username);
+            pass = (EditText) findViewById(R.id.et_password);
+            btnRegis = (FloatingActionButton) findViewById(R.id.floatregis);
+            btnLogin = (Button) findViewById(R.id.btnLogin);
+
+            btnLogin.setOnClickListener(this);
+            btnRegis.setOnClickListener(this);
+        }
 
     @Override
-    protected void onCreate(Bundle savedIntanceState){
-        super.onCreate(savedIntanceState);
-        setContentView(R.layout.register_main);
+    public void onClick(View v) {
+        if(v == btnLogin){
+            Intent intent = new Intent(registerActivity. this, loginActivity.class);
+            startActivity(intent);
+        }else if(v == btnRegis){
+            String username = user.getText().toString().trim();
+            String password = pass.getText().toString().trim();
 
-        Username = (EditText)findViewById(R.id.et_username);
-        Password = (EditText)findViewById(R.id.et_password);
-        Login = (Button) findViewById(R.id.btnLogin);
-        Register = (TextView)findViewById(R.id.tv_title);
-
-
-        Button btnRegis = findViewById(R.id.btnLogin);
-        btnRegis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (v == Register) {
-                    Intent goRegister = new Intent(registerActivity.this, loginActivity.class);
-                    startActivity(goRegister);
-                    finish();
-                } else if (v == btnRegis) {
-                    String username = Username.getText().toString().trim();
-                    String password = Password.getText().toString().trim();
-
-                }
-                Username.setError("Masukkan Username");
-                Password.setError("Masukkan Password");
-
+            if(!username.isEmpty() && !password.isEmpty()){
+                if(password.equals(password))Register();
+                else pass.setError("Masukkan Password");
+            }else{
+                user.setError("Masukkan Username");
+                pass.setError("Masukkan Password");
             }
 
-        });
+        }
     }
+
     private void Register() {
-        Register.setVisibility(View.GONE);
+            btnRegis.setVisibility(View.GONE);
 
-        final String Username = this.Username.getText().toString().trim();
-        final String Password= this.Password.getText().toString().trim();
+            final String username = this.user.getText().toString().trim();
+            final String password = this.pass.getText().toString().trim();
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,URL_REGIST,
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -79,38 +82,34 @@ public class registerActivity extends AppCompatActivity {
                                 Toast.makeText(registerActivity.this, pesan, Toast.LENGTH_SHORT).show();
                                 Intent berhasil = new Intent(registerActivity.this, loginActivity.class);
                                 startActivity(berhasil);
-                                Register.setVisibility(View.VISIBLE);
+                                btnRegis.setVisibility(View.VISIBLE);
                             } else {
-                                Toast.makeText(registerActivity.this, pesan, Toast.LENGTH_SHORT).show();
-                                Register.setVisibility(View.VISIBLE);
-
+                                Toast.makeText(registerActivity.this, "", Toast.LENGTH_SHORT).show();
+                                btnRegis.setVisibility(View.VISIBLE);
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
-                            Toast.makeText(registerActivity.this, "Register Error!" + e.toString(), Toast.LENGTH_SHORT).show();
-                            Register.setVisibility(View.VISIBLE);
+                            Toast.makeText(registerActivity.this, "Registrasi Erorr!"+e.toString(), Toast.LENGTH_SHORT).show();
+                            btnRegis.setVisibility(View.VISIBLE);
                         }
                     }
-                }, new Response.Listener<String>() {
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(registerActivity.this, "Registrasi Erorr!" +error.toString(), Toast.LENGTH_SHORT).show();
+                        btnRegis.setVisibility(View.VISIBLE);
+                    }
+                }){
             @Override
-            public void onResponse(String response) {
-                Toast.makeText(registerActivity.this, "Register Error!" + error.toString(), Toast.LENGTH_SHORT).show();
-                Register.setVisibility(View.VISIBLE);
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String> params = new HashMap<>();
+                params.put("user",username);
+                params.put("pass",password);
+                return params;
             }
-        }){
-            @Override
-            protected Map<String,String> getParams()throws AuthFailureError{
-                Map<String, String> params= new HashMap<>();
-                params.put("Username",Username);
-                params.put("Password",Password);
-               return params;
-            }
-
         };
-
-
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
-
     }
 }
