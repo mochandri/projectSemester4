e6158d3b48dcfc2a07e0cff4e0631c26bdc499f5
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-06-02 19:21:36 +0700

    update

--- rizky/app/src/main/java/com/example/rizky/registerActivity.java
+++ rizky/app/src/main/java/com/example/rizky/registerActivity.java
@@ -4,24 +4,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class registerActivity extends AppCompatActivity {
+
+    private EditText Username, Password;
+    private Button Login;
+    private TextView Register;
+
     @Override
     protected void onCreate(Bundle savedIntanceState){
         super.onCreate(savedIntanceState);
         setContentView(R.layout.register_main);
 
+        Username = (EditText)findViewById(R.id.et_username);
+        Password = (EditText)findViewById(R.id.et_password);
+        Login = (Button) findViewById(R.id.btnLogin);
+        Register = (TextView)findViewById(R.id.tv_title);
+
         Button btnRegis = findViewById(R.id.btnLogin);
         btnRegis.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent goRegister = new Intent(registerActivity.this, loginActivity.class);
-                startActivity(goRegister);
-                finish();
+                if(v==Register){
+                    Intent goRegister = new Intent(registerActivity.this, loginActivity.class);
+                    startActivity(goRegister);
+                    finish();
+                }else if(v==btnRegis){
+                    String username = Username.getText().toString().trim();
+                    String password = Password.getText().toString().trim();
+                if(!username.isEmpty()&& !password.isEmpty()){
+                    if (equals(password))Register();
+                    else password.setError("konfirmasi password salahh");
+
+                }
+                Username.setError("Masukkan Username");
+                Password.setError("Masukkan Password");
+
+                }
+
 
             }
         });
+    }
+
+    private void Register() {
+        loading.setVisibilty(View.VISIBLE);
+        Register.setVisibility(View.GONE);
+
     }
 }
