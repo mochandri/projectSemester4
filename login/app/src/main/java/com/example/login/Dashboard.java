61fd5ee336e7ce61816b170a3d9f46d270b3385b
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-06-05 02:53:18 +0700

    Dashboard

--- login/app/src/main/java/com/example/login/Dashboard.java
+++ login/app/src/main/java/com/example/login/Dashboard.java
@@ -3,12 +3,40 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Dashboard extends AppCompatActivity {
-
+    private  final String TAG = "Dashboard";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        getDataFromApi();
     }
+    private  void getDataFromApi(){
+        DBcontract.endpoint().getData()
+                .enqueue(new Callback<MainModel>() {
+                    @Override
+                    public void onResponse(Call<MainModel> call, Response<MainModel> response) {
+                        if(response.isSuccessful()){
+                            List<MainModel.Result> results = response.body().getResult();
+                            Log.d(TAG, results.toString());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<MainModel> call, Throwable t) {
+                        Log.d(TAG, t.toString());
+
+                    }
+                });
+    }
+
+
 }
