11399b15f5807dd081f28ac9f67af1733cf33c4a
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-06-05 17:12:39 +0700

    menampilkan data gambar

---
+++ login/app/src/main/java/com/example/login/DetailActivity.java
@@ -0,0 +1,27 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+    private  final  String TAG ="DetailActivity";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        String nama_brg = getIntent(),getStringExtra("intent_nama_brg");
+        String image = getIntent(),getStringExtra("intent_image");
+        getSupportActionBar().setTitle(title);
+        Picasso.get()
+                .load(image)
+                .into((ImageView) findViewById(R.id.imageView));
+    }
+}
--- login/app/src/main/java/com/example/login/MainAdapter.java
+++ login/app/src/main/java/com/example/login/MainAdapter.java
@@ -9,15 +9,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
 
-    private List<MainModel.Result> results = new ArrayList<>();
+    private List<MainModel.Result> results ;
+    private  OnAdapterListener listener ;
 
-    public MainAdapter(List<MainModel.Result> results) {
+    public MainAdapter(List<MainModel.Result> results, OnAdapterListener listener) {
         this.results = results;
+        this.listener = listener;
     }
 
     @NonNull
@@ -32,6 +36,16 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         MainModel.Result result = results.get(position);
         holder.textView.setText( result.getTitle());
+        Picasso.get()
+                .load(result.getImage())
+                .fit().centerCrop()
+                .into(holder.imageView);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onClick(result);
+            }
+        });
 
     }
 
@@ -57,6 +71,9 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
         results.clear();
         results.addAll(data);
         notifyDataSetChanged();
+    }
+    interface OnAdapterListener{
+        void onClick(MainModel.Result result);
     }
 }
 
